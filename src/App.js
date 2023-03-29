aeb809677ceacebb3cab7b37d858b21d59e893b6
Stan454 <99740736+Stan454@users.noreply.github.com>
2023-03-29 10:26:18 +0200

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import NavBar from './Components/NavBar';
+import Dishes from './Pages/Dishes';
+import Home from './Pages/Home';
+import Reservations from './Pages/Reservations';
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <NavBar />
+    <div className="container">
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/Dishes' element={<Dishes />} />
+        <Route path='/Reservations' element={<Reservations />} />
+      </Routes>
     </div>
-  );
+    </>
+  )
 }
 
 export default App;
