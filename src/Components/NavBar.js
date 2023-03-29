81a81e17a3e14ff1ca63d1e0c369c018c31eaa1c
Stan454 <99740736+Stan454@users.noreply.github.com>
2023-03-29 10:27:57 +0200

    Add hover function

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -15,7 +15,7 @@ function CustomLink({to, children, ...props}){
     const resolvedPath = useResolvedPath(to)
     const isActive = useMatch({ path: resolvedPath.pathname, end : true})
     return(
-        <li classname={isActive ? "active" : ""}>
+        <li className={isActive ? "active" : ""}>
             <Link to={to} {...props}>
                 {children}
             </Link>
